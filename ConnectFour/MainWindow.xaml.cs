1c6d939df382c7e3e92f22487ee3f29903d9598e
Tree-0 <117487029+Tree-0@users.noreply.github.com>
2024-08-15 20:40:27 -0500

    Add project files.

---
+++ ConnectFour/MainWindow.xaml.cs
@@ -0,0 +1,149 @@
+﻿using ConnectFour.Model;
+using Microsoft.Windows.Themes;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Security.Policy;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ConnectFour
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        // model of board, containing all tokens
+        public Board Board { get; set; }
+
+        /// <summary>
+        /// TurnNumber: the number of turns played in the current game
+        /// </summary>
+        private int _turnNumber;
+        public int TurnNumber
+        {
+            get { return _turnNumber; }
+            set
+            {
+                if (_turnNumber != value)
+                {
+                    _turnNumber = value;
+                    OnPropertyChanged(nameof(TurnNumber));
+                }
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+
+            // Init model
+            Board = new Board();
+
+            // //Test
+            // Board.PlaceToken(0, 'Y');
+            // Board.PlaceToken(1, 'R');
+            // Board.PlaceToken(1, 'Y');
+            // Debug.WriteLine(Board.ToString());
+            // Debug.WriteLine(Board.EmptyRows);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        /// <summary>
+        /// Make the previewed move final- place token, increment turn counter, switch teams
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ConfirmMoveButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Move finished, increase turn count
+            TurnNumber++;
+        }
+
+
+        /// <summary>
+        /// Remove all tokens from the board and set turn counter to zero
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void RestartButton_Click(object sender, RoutedEventArgs e)
+        {
+            // New game, Turn = 0
+            TurnNumber = 0;
+        }
+
+
+        /// <summary>
+        /// Deselect Cell and remove token preview
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CancelMoveButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        /// <summary>
+        /// When a cell is clicked, change the border color and place a token
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GridCell_GotFocus(object sender, RoutedEventArgs e)
+        {
+            Button clickedButton = sender as Button;
+
+            if (clickedButton != null)
+            {
+                Border selectedBorder = clickedButton.Parent as Border;
+
+                // Highlight the selected border
+                if (selectedBorder != null)
+                {
+                    selectedBorder.BorderBrush = Brushes.Red;
+                    selectedBorder.Background = Brushes.LightGray;
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// When a different cell is clicked, revert the de-selected cell to its original state
+        /// </summary>
+        /// <param name="sender"></param> 
+        /// <param name="e"></param>
+        private void GridCell_LostFocus(object sender, RoutedEventArgs e)
+        {
+            Button clickedButton = sender as Button;
+
+            if (clickedButton != null)
+            {
+                Border selectedBorder = clickedButton.Parent as Border;
+
+                // Un-highlight the de-selected border
+                if (selectedBorder != null)
+                {
+                    selectedBorder.BorderBrush= Brushes.Black;
+                    selectedBorder.Background = Brushes.Transparent;
+                }
+
+            }
+        }
+    }
+}
---
+++ ConnectFour/Model/Board.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectFour.Model
+{
+    public class Board
+    {
+        public char[,] Tokens;
+
+        public int Height { get; }
+
+        public int Width { get; }
+
+        // number of empty rows at the top of the board
+        // convenience property for later
+        public int EmptyRows { get; set; }
+
+
+
+        public Board(int height=6, int width=7)
+        {
+            Height = height; Width = width;
+            Tokens = new char[height, width];
+
+            EmptyRows = height;
+
+            // Initialize the board with default '-'
+            for (int row = 0; row < Height; row++)
+            {
+                for (int col = 0; col < Width; col++)
+                {
+                    Tokens[row, col] = '-';
+                }
+            }
+        }
+
+        /// <summary>
+        /// Place the designated player's token in the designated column
+        /// </summary>
+        /// <param name="col"></param> 
+        ///     0 < col < Board.Width
+        /// <param name="player"></param>
+        ///     player is 'R' or 'Y'
+        public void PlaceToken(int col, char player)
+        {
+            for (int row = Tokens.GetLength(0) - 1; row >= 0; row--)
+            {
+                if (Tokens[row, col] == '-')
+                {
+                    // check if token was placed in empty row
+                    EmptyRows = Math.Min(EmptyRows, row);
+
+                    Tokens[row, col] = player; 
+                    break;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            
+            for (int row  = 0; row < Height; row++)
+            {
+                for (int col = 0; col < Width; col++)
+                {
+                    sb.Append(Tokens[row, col]);
+                    if (col < Width - 1)
+                        sb.Append(',');
+                }
+
+                sb.Append('\n');
+            }
+            
+            return sb.ToString();
+        }
+    }
+}
